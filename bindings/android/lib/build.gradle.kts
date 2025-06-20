1d7f19fd0a1f2232a097c5b2bac76020fe197d31
Ovi Trif <ovitrif@proton.me>
2025-06-20 13:16:52 +0200

    fix: android compatibility with 16 KB devices
    Android warns about this and will require it from Nov 2025
    see https://developer.android.com/16kb-page-size

--- bindings/android/lib/build.gradle.kts
+++ bindings/android/lib/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-    implementation("net.java.dev.jna:jna:5.13.0@aar")
+    implementation("net.java.dev.jna:jna:5.17.0@aar")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("androidx.appcompat:appcompat:1.6.1")
