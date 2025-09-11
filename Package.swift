0f90ca86c8eba6a06b0a5098816696c9dcf69332
coreyphillips <corey.lyle.phillips@proton.me>
2025-09-10 20:08:53 -0400

    chore: upgrade rust blocktank client
    - Upgrades rust-blocktank-client to 0.0.14
    - Upgrades bitkit-core version to 0.1.13
    - Updates bindings accordingly

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let tag = "v0.1.12"
-let checksum = "96142b6030d42d7b0ac9d26efa9a5a1dd0ce11e62e4ac8e52af663cbfbdd24b1"
+let tag = "v0.1.13"
+let checksum = "314f272638f1e3e9d074e199eb2707b1b329c5b9b8e5454c83a7ca5ace68feb6"
 let url = "https://github.com/synonymdev/bitkit-core/releases/download/\(tag)/BitkitCore.xcframework.zip"
 
 let package = Package(
